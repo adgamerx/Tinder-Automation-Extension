6422bab5a205fca6651e9076ea8fe5a3f0bec2c6
Aman Dutt <amank.u.m100@gmail.com>
2020-10-12 14:54:01 +0530

    only active on tinder

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener(function (request) {
+    if (request.do == "extensionActive") {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        },
+            function (tabs) {
+                chrome.pageAction.show(tabs[0].id);
+            })
+    }
+})
