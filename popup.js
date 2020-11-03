27cfb27b344dfba950f3321e075913e9954d58fb
Aman Dutt <amank.u.m100@gmail.com>
2020-11-03 22:45:46 +0530

    removed useless codes

--- popup.js
+++ popup.js
@@ -1,29 +1,3 @@
-
-   
-// var a = document.getElementById("like");
-// var b = document.getElementById("nope");
-// var c = document.getElementById("stop");
-
-// function startLike(){
-//     console.log("started liking")
-//     setInterval( function likeit(){
-//         var likeBtn = document.querySelector('[aria-label="Like"]'); likeBtn.click() }, 1000)
-// }
-
-// function startNope(){
-//     console.log("started nope")
-//     setInterval( function nopeit(){
-//         var likeBtn = document.querySelector('[aria-label="Nope"]'); likeBtn.click() }, 1000)
-// }
-
-// function stop(){
-
-// }
-
-// a.addEventListener('click', startLike);
-// b.addEventListener('click', startNope);
-// c.addEventListener('click', stop);
-
 function like() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
     chrome.tabs.sendMessage(tabs[0].id, {message: "like"});
