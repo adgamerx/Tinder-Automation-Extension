31cc9b2c0a9d2898b4494d6fa021e6ac91546daa
Aman Dutt <amank.u.m100@gmail.com>
2020-11-03 22:45:19 +0530

    added context and popup

--- content.js
+++ content.js
@@ -1,40 +1,49 @@
 // chrome.runtime.sendMessage({do: "extensionActive"});
 
 
-   
+
 // var a = document.getElementById("like");
 // var b = document.getElementById("nope");
 // var c = document.getElementById("stop");
 
-// function startLike(){
-//     console.log("started liking")
-//     setInterval( function likeit(){
-//         var likeBtn = document.querySelector('[aria-label="Like"]'); likeBtn.click() }, 1000)
-// }
-
-// function startNope(){
-//     console.log("started nope")
-//     setInterval( function nopeit(){
-//         var likeBtn = document.querySelector('[aria-label="Nope"]'); likeBtn.click() }, 1000)
-// }
-
-// function stop(){
-
-// }
 
 // a.addEventListener('click', startLike);
 // b.addEventListener('click', startNope);
 // c.addEventListener('click', stop);
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if(request.message === "start" ) {
-       start();
-       console.log("ext loaded");
-           }
+  function (request, sender, sendResponse) {
+    if (request.message === "like") {
+      startLike();
     }
-  );
-
-  function start(){
-      alert("you just clicked me");
+    if (request.message === "nope") {
+      startNope();
+    }
+    if (request.message === "pause") {
+      pause();
+    }
   }
+);
+
+function startLike(){
+    console.log("started liking");
+    setInterval( likeit(), 1000);
+    function likeit(){
+    var likeBtn = document.querySelector('[aria-label="Like"]'); likeBtn.click() }
+}
+
+function startNope(){
+    console.log("started nope")
+    setInterval(nopeit(), 1000)
+    function nopeit(){
+    var nopeBtn = document.querySelector('[aria-label="Nope"]'); nopeBtn.click() }
+}
+
+function pause(){
+  clearInterval(likeit);
+  clearInterval(nopeit);  
+  console.log("Automation Stopped");
+}
+// function start() {
+  //   alert("you just clicked me");
+// }
--- popup.js
+++ popup.js
@@ -24,12 +24,24 @@
 // b.addEventListener('click', startNope);
 // c.addEventListener('click', stop);
 
-function popup() {
+function like() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {message: "start"});
+    chrome.tabs.sendMessage(tabs[0].id, {message: "like"});
+   });
+}
+function nope() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {message: "nope"});
+   });
+}
+function stop() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {message: "pause"});
    });
 }
 
 // document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("button1").addEventListener('click' , popup);
+  document.getElementById("like").addEventListener('click' , like());
+  document.getElementById("nope").addEventListener('click' , nope());
+  document.getElementById("pause").addEventListener('click' , stop());
 // });
