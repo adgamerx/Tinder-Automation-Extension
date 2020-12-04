bf4c90575022204ff5c06cf2449812a862b13848
ADGAMERX <amank.u.m100@gmail.com>
2020-12-04 19:06:41 +0530

    fixed new tinder version issue

--- content.js
+++ content.js
@@ -1,8 +1,19 @@
+// Old Code 
+
+// let likeit = () => {
+//   var likeBtn = document.querySelector('[aria-label="Like"]'); likeBtn.click()
+// }
+
+// let nopeit = () => {
+//   var dislikeBtn = document.querySelector('[aria-label="Nope"]'); dislikeBtn.click()
+// }
+
 let likeit = () => {
-  var likeBtn = document.querySelector('[aria-label="Like"]'); likeBtn.click()
+  var likeBtn = document.querySelector("#content > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.recsCardboard.W\\(100\\%\\).Mt\\(a\\).H\\(100\\%\\)--s.Px\\(4px\\)--s.Pos\\(r\\) > div > div.Pos\\(r\\).Py\\(16px\\).Py\\(12px\\)--s.Px\\(4px\\).Px\\(8px\\)--ml.D\\(f\\).Jc\\(sb\\).Ai\\(c\\).Maw\\(375px\\)--m.Mx\\(a\\).Pe\\(n\\).Mt\\(-1px\\) > div:nth-child(4) > button"); likeBtn.click()
 }
+
 let nopeit = () => {
-  var dislikeBtn = document.querySelector('[aria-label="Nope"]'); dislikeBtn.click()
+  var dislikeBtn = document.querySelector("#content > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.recsCardboard.W\\(100\\%\\).Mt\\(a\\).H\\(100\\%\\)--s.Px\\(4px\\)--s.Pos\\(r\\) > div > div.Pos\\(r\\).Py\\(16px\\).Py\\(12px\\)--s.Px\\(4px\\).Px\\(8px\\)--ml.D\\(f\\).Jc\\(sb\\).Ai\\(c\\).Maw\\(375px\\)--m.Mx\\(a\\).Pe\\(n\\).Mt\\(-1px\\) > div:nth-child(2) > button"); dislikeBtn.click()
 }
 
 var auto;
